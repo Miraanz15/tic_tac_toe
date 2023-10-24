0762411dca8a0d5011dfca2a7bcf24e8ec682d96
Miraanz15 <miraanejaz15@gmail.com>
2023-10-25 02:00:54 +0530

    Array added

--- src/Board.js
+++ src/Board.js
@@ -3,36 +3,29 @@ import Square from './Square';
 import styles from './Board.module.css';
 
 const Board = () => {
-  const [value, setValue] = useState(Array(9).fill(null));
-  const [display, setDisplay] = useState('X');
-  console.log("Value = " + value);
+  const [state, setState] = useState(Array(9).fill(null));
+  
+  const handleClick = (index) => {
 
-
-  const onClick = () => {
-     if(display === 'X'){
-        setDisplay('O');
-     }
-     else{
-        setDisplay('X');
-     }
   }
+
 
   return (
     <div className={styles.board}>
         <div className={styles.board_row}>
-            <Square value={'X'}/>
-            <Square value={'X'}/>
-            <Square value={'X'}/>
+            <Square value={state[0]} onClick = {() => handleClick(0)}/>
+            <Square value={state[1]} onClick = {() => handleClick(1)}/>
+            <Square value={state[2]} onClick = {() => handleClick(2)}/>
         </div>
         <div className={styles.board_row}>
-            <Square value={'X'}/>
-            <Square value={'X'}/>
-            <Square value={'X'}/>
+            <Square value={state[3]} onClick = {() => handleClick(3)}/>
+            <Square value={state[4]} onClick = {() => handleClick(4)}/>
+            <Square value={state[5]} onClick = {() => handleClick(5)}/>
         </div>
         <div className={styles.board_row}>
-            <Square value={'X'}/>
-            <Square value={'X'}/>
-            <Square value={'X'}/>
+            <Square value={state[6]} onClick = {() => handleClick(6)}/>
+            <Square value={state[7]} onClick = {() => handleClick(7)}/>
+            <Square value={state[8]} onClick = {() => handleClick(8)}/>
         </div>
     </div>
   )
